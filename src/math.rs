e24ab13375415f89552b14490d387079896b4b2b
neuring <neuring@pm.me>
2021-11-26 21:10:02 +0100

    fix: early exit gcd calc if gcd is one

--- src/math.rs
+++ src/math.rs
@@ -4,16 +4,14 @@ pub fn gcd<N: Numeric>(result: &mut N, values: &[N]) {
     result.clone_from(&values[0]);
 
     for val in values.iter().skip(1) {
+        if result == &1 {
+            return;
+        }
         result.gcd_assign(val);
     }
 }
 
-pub fn special_mod<N: Numeric>(
-    result: &mut N,
-    rounded_div: &mut N,
-    a: &N,
-    b: &N,
-) {
+pub fn special_mod<N: Numeric>(result: &mut N, rounded_div: &mut N, a: &N, b: &N) {
     rounded_divisor(rounded_div, a, b, result);
 
     result.clone_from(rounded_div);
@@ -24,12 +22,7 @@ pub fn special_mod<N: Numeric>(
 
 /// calculates ⌊a / b + 1/2 ⌋, but without using floats
 /// ⌊a / b + 1/2 ⌋ = ⌊(a + 2 * b) / (2 * b)⌋
-pub fn rounded_divisor<N: Numeric>(
-    result: &mut N,
-    a: &N,
-    b: &N,
-    scratch: &mut N,
-) {
+pub fn rounded_divisor<N: Numeric>(result: &mut N, a: &N, b: &N, scratch: &mut N) {
     result.clone_from(a);
     *result *= 2;
     *result += b;
