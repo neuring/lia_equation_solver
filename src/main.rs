36a77bba1d3f67f9d2773016ee0ea5cd48583903
neuring <neuring@pm.me>
2021-12-07 19:10:15 +0100

    feat: added cli option to convert to smtlib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use anyhow::Context;
 use structopt::StructOpt;
 
 use crate::system::{System, VariableIndex};
@@ -13,24 +14,36 @@ mod util;
 
 #[derive(Debug, StructOpt)]
 struct Config {
+    #[structopt(subcommand)]
+    command: Command,
+
+    /// Path to input file
+    input: PathBuf,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Convert input to smt-lib format
+    ToSMTLib { output: Option<PathBuf> },
+
+    /// Solve system of equation.
+    Solve(SolverConfig),
+}
+
+#[derive(Debug, StructOpt)]
+struct SolverConfig {
+    /// dump solution as dot graph
     #[structopt(short, long)]
     dump_dot: Option<PathBuf>,
 
+    /// Do not verify the solution
     #[structopt(short, long)]
     no_verify: bool,
-
-    input: PathBuf,
 }
 
 type N = rug::Integer;
 
-fn main() -> anyhow::Result<()> {
-    let config = Config::from_args();
-
-    let input = std::fs::read_to_string(config.input)?;
-
-    let mut system: System<N> = parser::parse(&input)?;
-
+fn solver_main(mut system: System<N>, config: &SolverConfig) -> anyhow::Result<()> {
     let original_system = system.clone();
 
     if system.starting_variables <= 10 {
@@ -42,7 +55,7 @@ fn main() -> anyhow::Result<()> {
     if result == algo::Result::Sat {
         println!("Solvable");
 
-        if let Some(dump_path) = config.dump_dot {
+        if let Some(dump_path) = config.dump_dot.as_ref() {
             let f = std::fs::File::create(dump_path)?;
             system
                 .reconstruction
@@ -88,5 +101,25 @@ fn main() -> anyhow::Result<()> {
         Ok(())
     } else {
         Err(anyhow::anyhow!(""))
+    }
+}
+
+fn smtlib_main(system: System<N>, out_path: Option<PathBuf>) -> anyhow::Result<()> {
+    todo!()
+}
+
+fn main() -> anyhow::Result<()> {
+    let config = Config::from_args();
+
+    let input = std::fs::read_to_string(&config.input).with_context(|| {
+        format!("Couldn't read file '{}'", &config.input.to_string_lossy())
+    })?;
+
+    let system: System<N> =
+        parser::parse(&input).context("Failed to parse input.")?;
+
+    match config.command {
+        Command::ToSMTLib { output } => todo!(),
+        Command::Solve(solver_config) => solver_main(system, &solver_config),
     }
 }
